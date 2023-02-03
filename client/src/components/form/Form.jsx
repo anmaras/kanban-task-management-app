637954dfc4f60737a4fb752a43124c8bed6e6357
Anthony Maras <marasantonis@gmail.com>
2023-02-03 20:40:54 +0200

    add formik props for errors

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -13,49 +13,59 @@ const initialValues = {
 const validationSchema = yup.object({
   name: yup
     .string()
-    .required('Required')
     .min(3, 'Name need to be at least 3 chars')
-    .max(20, 'Name cant be more than 20 chars'),
+    .max(20, 'Name cant be more than 20 chars')
+    .required('Required'),
   email: yup.string().email('Invalid email format').required('Required'),
   password: yup
     .string()
-    .required('Required')
-    .min(6, 'Password need to be at least 6 chars'),
+    .min(6, 'Password need to be at least 6 chars')
+    .required('Required'),
 });
 
 const onSubmit = (values) => console.log(values);
 const onSubmit2 = (values) => console.log(values, 'Test');
 
 const Form = ({ formType, path }) => {
+  //create form from formInput array check constants.jsx
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
-      //check the prop and use the corresponding function
       onSubmit={formType === 'register' ? onSubmit : onSubmit2}
+      touched
     >
-      <FormikForm className="form">
-        {formInputs
-          .map((input) => {
-            const { name, label, type } = input;
-            return (
-              <div key={name} className="form-control">
-                <label htmlFor={name}>{label}</label>
-                <Field type={type} name={name} id={name} />
-                <ErrorMessage name={name} component="div" className="error" />
-              </div>
-            );
-          })
-          .filter((input) => {
-            /* If the formType is register then return the form input array
-           as it is, if not then filter it so it return only the email and password */
-            if (formType === 'register') {
-              return input;
-            }
-            return input.key !== 'name';
-          })}
-        <button type="submit">submit</button>
-      </FormikForm>
+      {({ touched, errors }) => (
+        <FormikForm className="form">
+          {formInputs
+            .map((input) => {
+              const { name, label, type } = input;
+              return (
+                <div key={name} className="form-control">
+                  <label htmlFor={name}>{label}</label>
+                  <Field
+                    type={type}
+                    name={name}
+                    id={name}
+                    className={
+                      touched[name] && errors[name] ? 'not-valid' : 'valid'
+                    }
+                  />
+                  <ErrorMessage name={name} component="div" className="error" />
+                </div>
+              );
+            })
+            .filter((input) => {
+              /* If the formType is register then return the form input array
+           as it is, if not then filter it so it return only the email and password input */
+              if (formType === 'register') {
+                return input;
+              }
+              return input.key !== 'name';
+            })}
+          <button type="submit">submit</button>
+        </FormikForm>
+      )}
     </Formik>
   );
 };
