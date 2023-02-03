d7cd9e4c2b7fb81a7cc03898b175e3a864c11b38
Anthony Maras <marasantonis@gmail.com>
2023-02-03 18:25:38 +0200

    pages initial commit

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Form } from '../components';
+
+const RegisterPage = () => {
+  return <Form type="register" path="/register" />;
+};
+
+export default RegisterPage;
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,4 @@
+import Homepage from './HomePage';
+import RegisterPage from './RegisterPage';
+
+export { Homepage, RegisterPage };
