f477ede04190ba8b0319fa043a4c59a95bec72b2
Anthony Maras <marasantonis@gmail.com>
2023-02-03 22:31:36 +0200

    add register function and alert

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -3,6 +3,7 @@ import { formInputs } from '../../utils/constants';
 import PropTypes from 'prop-types';
 import { Formik, Form as FormikForm, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
+import { useUserContext } from '../../context/userContext';
 
 const initialValues = {
   name: '',
@@ -23,56 +24,64 @@ const validationSchema = yup.object({
     .required('Required'),
 });
 
-const onSubmit = (values) => console.log(values);
 const onSubmit2 = (values) => console.log(values, 'Test');
 
-const Form = ({ formType, path }) => {
+const Form = ({ formType }) => {
+  const { registerUser, showAlert, alertText } = useUserContext();
   //create form from formInput array check constants.jsx
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={formType === 'register' ? onSubmit : onSubmit2}
-      touched
-    >
-      {({ touched, errors }) => (
-        <FormikForm className="form">
-          {formInputs
-            .map((input) => {
-              const { name, label, type } = input;
-              return (
-                <div key={name} className="form-control">
-                  <label htmlFor={name}>{label}</label>
-                  <Field
-                    type={type}
-                    name={name}
-                    id={name}
-                    className={
-                      touched[name] && errors[name] ? 'not-valid' : 'valid'
-                    }
-                  />
-                  <ErrorMessage name={name} component="div" className="error" />
-                </div>
-              );
-            })
-            .filter((input) => {
-              /* If the formType is register then return the form input array
+    <div>
+      <h2>{formType === 'register' ? 'Register' : 'Login'}</h2>
+      {showAlert ? <div className="alert-message">{alertText}</div> : null}
+
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={formType === 'register' ? registerUser : onSubmit2}
+        touched
+      >
+        {({ touched, errors }) => (
+          <FormikForm className="form">
+            {formInputs
+              .map((input) => {
+                const { name, label, type } = input;
+                return (
+                  <div key={name} className="form-control">
+                    <label htmlFor={name}>{label}</label>
+                    <Field
+                      type={type}
+                      name={name}
+                      id={name}
+                      className={
+                        touched[name] && errors[name] ? 'not-valid' : 'valid'
+                      }
+                    />
+                    <ErrorMessage
+                      name={name}
+                      component="div"
+                      className="error"
+                    />
+                  </div>
+                );
+              })
+              .filter((input) => {
+                /* If the formType is register then return the form input array
            as it is, if not then filter it so it return only the email and password input */
-              if (formType === 'register') {
-                return input;
-              }
-              return input.key !== 'name';
-            })}
-          <button type="submit">submit</button>
-        </FormikForm>
-      )}
-    </Formik>
+                if (formType === 'register') {
+                  return input;
+                }
+                return input.key !== 'name';
+              })}
+            <button type="submit">submit</button>
+          </FormikForm>
+        )}
+      </Formik>
+    </div>
   );
 };
 
 Form.propTypes = {
   formType: PropTypes.string.isRequired,
-  // path: PropTypes.string.isRequired,
 };
 
 export default Form;
