722416f2ac474d5a3b701a32ec04f41756eeea98
Anthony Maras <marasantonis@gmail.com>
2023-02-04 14:52:50 +0200

    add login controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,10 @@
 import User from '../models/Users.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError, NotFoundError } from '../errors/index.js';
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+} from '../errors/index.js';
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -27,7 +31,36 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+
+  //check if both values are valid
+  if (!email || !password) {
+    throw new BadRequestError('All values needed');
+  }
+
+  /* Because of the select:false property in model the password
+  wont return in the document so with select('+password') i override it */
+  const user = await User.findOne({ email }).select('+password');
+
+  //if user doesn't exist throw custom error
+  if (!user) {
+    throw new UnauthenticatedError('Invalid email');
+  }
+  //compare passwords, method is created at user model
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  // if password not match throw custom error
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Password');
+  }
+
+  //create token/ method comes from user model check there.
+  const token = user.createJWT();
+
+  //remove password from response
+  user.password = undefined;
+
+  res.status(StatusCodes.OK).json({ user, token });
 };
 
 const updateUser = async (req, res) => {
