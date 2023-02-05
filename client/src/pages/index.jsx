1aa96d18c44c87c448a49506aac83a90ef310914
Anthony Maras <marasantonis@gmail.com>
2023-02-05 14:24:55 +0200

    update index

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,5 +1,7 @@
 import Homepage from './HomePage';
 import RegisterPage from './RegisterPage';
 import LoginPage from './LoginPage';
+import DashboardPage from './DashboardPage';
+import ProtectedRoute from './ProtectedRoute';
 
-export { Homepage, RegisterPage, LoginPage };
+export { Homepage, RegisterPage, LoginPage, DashboardPage, ProtectedRoute };
