2104fc616cab59fea9a1931c621973f31168b723
Anthony Maras <marasantonis@gmail.com>
2023-02-05 14:25:06 +0200

    set up basic router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,29 @@
+import {
+  Homepage,
+  RegisterPage,
+  LoginPage,
+  DashboardPage,
+  ProtectedRoute,
+} from './pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
-  return;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
