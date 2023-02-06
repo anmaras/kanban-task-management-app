54df258cb2ef939e92414690ed3f4f01973d0216
Anthony Maras <marasantonis@gmail.com>
2023-02-06 17:55:54 +0200

    set up toast functions

--- client/src/context/userContext.jsx
+++ client/src/context/userContext.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useReducer } from 'react';
 import reducer from '../reducers/userReducer';
 import axios from 'axios';
-// import { toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import {
   REGISTER_USER_BEGIN,
   REGISTER_USER_SUCCESS,
@@ -9,7 +9,6 @@ import {
   LOGIN_USER_BEGIN,
   LOGIN_USER_SUCCESS,
   LOGIN_USER_ERROR,
-  CLEAR_ALERT,
   LOGOUT_USER,
 } from '../utils/actions';
 
@@ -18,22 +17,38 @@ const token = localStorage.getItem('token');
 
 export const initialState = {
   isLoading: false,
-  showAlert: false,
-  alertText: '',
-  alertType: '',
   user: user ? JSON.parse(user) : null,
   token: token || null,
 };
+
+const notifySuccess = (text) =>
+  toast.success(text, {
+    position: 'bottom-center',
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
+
+const notifyError = (text) =>
+  toast.error(text, {
+    position: 'bottom-center',
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
 
 const UserContext = React.createContext();
 
 export const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  // const notify = (text) => toast.success(text);
-
-  const clearAlert = () => {
-    dispatch({ type: CLEAR_ALERT });
-  };
 
   const saveUserAtStorage = ({ user, token }) => {
     localStorage.setItem('user', JSON.stringify(user));
@@ -53,16 +68,18 @@ export const UserProvider = ({ children }) => {
       const { user, token } = response.data;
       dispatch({ type: REGISTER_USER_SUCCESS, payload: { user, token } });
       saveUserAtStorage({ user, token });
+      notifySuccess(`${user.name}, welcome! ❤️`);
     } catch (error) {
       //use the error msg from auth register controller
       dispatch({
         type: REGISTER_USER_ERROR,
         payload: { msg: error.response.data.msg },
       });
+      notifyError(error.response.data.msg);
     }
-    clearAlert();
   };
 
+  //login user
   const loginUser = async (currentUser) => {
     dispatch({ type: LOGIN_USER_BEGIN });
     try {
@@ -70,19 +87,21 @@ export const UserProvider = ({ children }) => {
       const { user, token } = response.data;
       dispatch({ type: LOGIN_USER_SUCCESS, payload: { user, token } });
       saveUserAtStorage({ user, token });
-      // notify('login');
+      notifySuccess(`${user.name},you manage to login gracefully`);
     } catch (error) {
       //use the error msg from auth login controller
       dispatch({
         type: LOGIN_USER_ERROR,
         payload: { msg: error.response.data.msg },
       });
+      notifyError(error.response.data.msg);
     }
-    clearAlert();
   };
 
+  //logout user
   const logoutUser = () => {
     dispatch({ type: LOGOUT_USER });
+    notifySuccess(`${state.user.name} has left the building 👋`);
     removeUserStorage();
   };
 
