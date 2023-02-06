0f1735e0f8b941cfeea2a73edeba0ea754e53113
Anthony Maras <marasantonis@gmail.com>
2023-02-06 16:02:30 +0200

    add middleware in user update route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { register, login, updateUser } from '../controllers/authController.js';
+import authenticatedUser from '../middleware/auth.js';
 
 router.route('/register').post(register);
 router.route('/login').post(login);
-router.route('/updateUser').patch(updateUser);
+router.route('/updateUser').patch(authenticatedUser, updateUser);
 
 export default router;
