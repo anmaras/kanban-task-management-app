dfbd577491a6cab799cae39189ba32bc766d7234
Anthony Maras <marasantonis@gmail.com>
2023-02-07 18:51:45 +0200

    update index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Form from './form/Form';
 import AuthLayout from './layouts/authLayout/AuthLayout';
+import HomeLayout from './layouts/homeLayout/HomeLayout';
 import Button from './button/Button';
 
-export { Form, Button, AuthLayout };
+export { Form, Button, AuthLayout, HomeLayout };
