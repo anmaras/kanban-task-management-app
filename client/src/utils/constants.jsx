c76d480ed8ab9e61928c99711ffe527213f9758c
Anthony Maras <marasantonis@gmail.com>
2023-02-07 15:13:34 +0200

    update auth form array

--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -3,15 +3,18 @@ export const formInputs = [
     label: 'Name',
     type: 'text',
     name: 'name',
+    placeholder: 'Your name here',
   },
   {
     label: 'Email',
     type: 'email',
     name: 'email',
+    placeholder: 'your@email.com',
   },
   {
     label: 'Password',
     type: 'password',
     name: 'password',
+    placeholder: 'Your password here',
   },
 ];
