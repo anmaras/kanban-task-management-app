73733904e4a6bd278e8ef26264c38de1e407d3d7
Anthony Maras <marasantonis@gmail.com>
2023-02-07 18:52:57 +0200

    set up landing page

---
+++ client/src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HomeLayout } from '../../components/index';
+import Typewriter from 'typewriter-effect';
+import style from './LandingPage.module.scss';
+
+const LandingPage = () => {
+  return (
+    <HomeLayout>
+      <div className={style.landingPage}>
+        <h1
+          className={[[style['landingPage__title']], 'heading-XXL'].join(' ')}
+        >
+          A visual system for managing work by using{' '}
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString('cards.')
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString('boards.')
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString('columns.')
+                .pauseFor(2500)
+                .deleteAll()
+                .start();
+            }}
+          />
+        </h1>
+        <p className={style['landingPage__subtitle']}>
+          Kanban will provide a simple yet effective solution for managing
+          projects and tasks.
+        </p>
+        <Link to="/register" className="button button--auth">
+          Get Started
+        </Link>
+      </div>
+    </HomeLayout>
+  );
+};
+
+export default LandingPage;
