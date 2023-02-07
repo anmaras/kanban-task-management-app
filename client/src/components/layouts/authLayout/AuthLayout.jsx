20f0b0152dc6b8835e4618bc5b0dd78b54570e03
Anthony Maras <marasantonis@gmail.com>
2023-02-07 18:50:50 +0200

    add link in title to get user to landing page

--- client/src/components/layouts/authLayout/AuthLayout.jsx
+++ client/src/components/layouts/authLayout/AuthLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './AuthLayout.module.scss';
+import { Link } from 'react-router-dom';
 
 const AuthLayout = ({ children }) => {
   return (
     <section className={style['layout']}>
       <header className={style['layout__header']}>
-        <h1 className={style['layout__title']}>kanban</h1>
+        <h1 className={style['layout__title']}>{<Link to="/">kanban</Link>}</h1>
       </header>
       <main className={style['layout__main']}>{children}</main>
     </section>
