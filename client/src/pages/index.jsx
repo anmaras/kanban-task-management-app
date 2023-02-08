2a8c00d8be2332a503c0ef348398dc996403cb34
Anthony Maras <marasantonis@gmail.com>
2023-02-08 20:46:31 +0200

    update index

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,7 +1,7 @@
 import LandingPage from './landingPage/LandingPage';
 import RegisterPage from './RegisterPage';
 import LoginPage from './LoginPage';
-import DashboardPage from './DashboardPage';
+import DashboardPage from './dashboardPage/DashboardPage';
 import ProtectedRoute from './ProtectedRoute';
 import ProfilePage from './ProfilePage';
 
