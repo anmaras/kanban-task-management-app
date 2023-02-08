40cbc1b369553e4753366a2ddb811aad8f042c9c
Anthony Maras <marasantonis@gmail.com>
2023-02-08 20:45:54 +0200

    update index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,18 @@
 import Form from './form/Form';
 import AuthLayout from './layouts/authLayout/AuthLayout';
 import HomeLayout from './layouts/homeLayout/HomeLayout';
-import Button from './button/Button';
-
-export { Form, Button, AuthLayout, HomeLayout };
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout';
+import DashboardHeader from './dashboard/dashboadHeader/DashboardHeader';
+import EditButton from './editButton/EditButton';
+import DashboardSide from './dashboard/dashboardSide/DashboardSide';
+import DashboardMain from './dashboard/dashboardMain/DashboardMain';
+export {
+  DashboardHeader,
+  DashboardLayout,
+  DashboardSide,
+  DashboardMain,
+  Form,
+  EditButton,
+  AuthLayout,
+  HomeLayout,
+};
