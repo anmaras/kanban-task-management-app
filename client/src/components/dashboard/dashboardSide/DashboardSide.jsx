8fe9145dfff0c73687fe2b61afcb9a2effdab3c0
Anthony Maras <marasantonis@gmail.com>
2023-02-09 18:20:07 +0200

    finalize hide button

--- client/src/components/dashboard/dashboardSide/DashboardSide.jsx
+++ client/src/components/dashboard/dashboardSide/DashboardSide.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import style from './DashboardSide.module.scss';
+import DashboardSideBoards from './DashboardSideBoards';
+import { ReactComponent as Eye } from '../../../assets/icon-hide-sidebar.svg';
 
 const DashboardSide = ({ isVisible, setVisibility }) => {
   return (
     <div className={isVisible ? style['side'] : style['side--hidden']}>
-      DashboardSide
-      <button onClick={setVisibility}>hide side</button>
+      <DashboardSideBoards />
+      <button onClick={setVisibility} className={style['side__hideBtn']}>
+        <Eye />
+        <span className="heading-M">Hide Sidebar</span>
+      </button>
     </div>
   );
 };
