60738c36358fffdbe9d9601217f385272f50cfab
Anthony Maras <marasantonis@gmail.com>
2023-02-09 18:21:58 +0200

    set up side boards

--- client/src/components/dashboard/dashboardSide/DashboardSideBoards.jsx
+++ client/src/components/dashboard/dashboardSide/DashboardSideBoards.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import style from './DashboardSideBoards.module.scss';
 import { ReactComponent as BoardIcon } from '../../../assets/icon-board.svg';
-const mokup = ['App Launch', 'Marketing Plan', 'RoadMap'];
+const mokup = [
+  'App Launch',
+  'Marketing Plan',
+  'RoadMap',
+  'App Launch',
+  'Marketing Plan',
+  'RoadMap',
+  'App Launch',
+  'Marketing Plan',
+  'RoadMap',
+  'App Launch',
+  'Marketing Plan',
+  'RoadMap',
+  'App Launch',
+  'Marketing Plan',
+  'RoadMap',
+  'App Launch',
+  'Marketing Plan',
+  'RoadMap',
+  'App Launch',
+  'Marketing Plan',
+  'RoadMap',
+  'App Launch',
+  'Marketing Plan',
+  'RoadMap',
+];
+
+/* one function for when user select a board 
+and another one when user need to create a new board */
 
 const DashboardSideBoards = () => {
   return (
     <div className={style.boards}>
-      <h3 className="heading-S">ALL BOARDS (3)</h3>
+      <h3 className={[style['boards__title'], 'heading-S'].join(' ')}>
+        ALL BOARDS (3)
+      </h3>
       <ul className={style['boards__list']}>
         {mokup.map((board, index) => {
           return (
-            <li className={style['boards__items']} key={index}>
+            <li className={style['boards__item']} key={index}>
               <BoardIcon />
               <h4 className="heading-M">{board}</h4>
             </li>
