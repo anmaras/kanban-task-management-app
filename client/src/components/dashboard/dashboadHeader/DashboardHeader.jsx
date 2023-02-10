724ece3010093665f9402501d191b1e9c26f2dc7
Anthony Maras <marasantonis@gmail.com>
2023-02-10 12:00:47 +0200

    add context functions for modal

--- client/src/components/dashboard/dashboadHeader/DashboardHeader.jsx
+++ client/src/components/dashboard/dashboadHeader/DashboardHeader.jsx
@@ -5,43 +5,51 @@ import { ReactComponent as AddTaskIcon } from '../../../assets/icon-add-task-mob
 import useWindowSize from '../../../hooks/useWindowSize';
 import style from './DashboardHeader.module.scss';
 import EditButton from '../../editButton/EditButton';
+import { useBoardContext } from '../../../context/boardsContext';
+import { Modal } from '../../index';
 
 //moke up array to test button
 const testArray = [1];
 
 const DashboardHeader = () => {
   const { width } = useWindowSize();
+  const { handleSideBoardModal, sideBoardModalVisible: isVisible } =
+    useBoardContext();
 
   return (
-    <header className={style.header}>
-      <div className={style['header__logo']}>
-        <Logo />
-        <h1 className={[style['header__appTitle']]}>kanban</h1>
-      </div>
-
-      <div className={style['header__controls']}>
-        <h2 className={[style['header__boardTitle'], 'heading-L'].join(' ')}>
-          board title
-        </h2>
-        <button
-          className={style['header__boardBtn']}
-          onClick={() => console.log('show board list')}
-        >
-          <span className="heading-L">board title</span>
-          <CevronDown />
-        </button>
-        <div className={style['header__buttons']}>
-          <button
-            className="button button--primary-L"
-            disabled={!testArray.length ? true : false}
-            onClick={() => console.log('test')}
-          >
-            {width < 768 ? <AddTaskIcon /> : 'Add New Task'}
-          </button>
-          <EditButton type="board" />
+    <>
+      {isVisible && <Modal type="showAllBoards" />}
+      <header className={style.header}>
+        <div className={style['header__logo']}>
+          <Logo />
+          <h1 className={[style['header__appTitle']]}>kanban</h1>
         </div>
-      </div>
-    </header>
+
+        <div className={style['header__controls']}>
+          <h2 className={[style['header__boardTitle'], 'heading-L'].join(' ')}>
+            board title
+          </h2>
+
+          <button
+            className={style['header__boardBtn']}
+            onClick={handleSideBoardModal}
+          >
+            <span className="heading-L">board title</span>
+            <CevronDown />
+          </button>
+          <div className={style['header__buttons']}>
+            <button
+              className="button button--primary-L"
+              disabled={!testArray.length ? true : false}
+              onClick={() => console.log('test')}
+            >
+              {width < 768 ? <AddTaskIcon /> : 'Add New Task'}
+            </button>
+            <EditButton type="board" />
+          </div>
+        </div>
+      </header>
+    </>
   );
 };
 
