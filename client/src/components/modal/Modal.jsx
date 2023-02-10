e55f64bc078d6dbf84d29ba732dac7a31fbe40af
Anthony Maras <marasantonis@gmail.com>
2023-02-10 12:01:13 +0200

    add close modal func to backdrop

--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import DashboardSideBoards from '../dashboard/dashboardSide/DashboardSideBoards';
 import style from './Modal.module.scss';
+import { useBoardContext } from '../../context/boardsContext';
 
 const Modal = ({ type }) => {
+  const { closeModal } = useBoardContext();
   if (type === 'showAllBoards') {
     return (
-      <div className={style.backdrop}>
+      <div className={style.backdrop} onClick={closeModal}>
         <DashboardSideBoards />
       </div>
     );
