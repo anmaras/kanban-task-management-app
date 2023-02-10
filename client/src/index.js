8fcab453fc96f016d11dafd0db2aca8a986797b3
Anthony Maras <marasantonis@gmail.com>
2023-02-10 12:01:45 +0200

    add board provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
 import { UserProvider } from './context/userContext';
+import { BoardProvider } from './context/boardsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <App />
+      <BoardProvider>
+        <App />
+      </BoardProvider>
     </UserProvider>
   </React.StrictMode>
 );
