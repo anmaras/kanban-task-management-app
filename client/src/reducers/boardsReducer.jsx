ccf12b153aed654b861c9ad01bc2ef510a2b74f3
Anthony Maras <marasantonis@gmail.com>
2023-02-11 18:49:13 +0200

    add and delete func added

--- client/src/reducers/boardsReducer.jsx
+++ client/src/reducers/boardsReducer.jsx
@@ -4,14 +4,24 @@ import {
   CREATE_BOARD_ERROR,
   SIDE_CREATE_MODAL_TOGGLE,
   CREATE_NEW_BOARD_MODAL_TOGGLE,
+  DELETE_MODAL_TOGGLE,
   CLOSE_MODAL,
   GET_USER_BOARD_BEGIN,
   GET_USER_BOARD_SUCCESS,
   GET_USER_BOARD_ERROR,
   GET_USER_BOARD_COLUMN_SUCCESS,
+  DELETE_BOARD_BEGIN,
+  DELETE_BOARD_SUCCESS,
+  DELETE_BOARD_ERROR,
+  EDIT_BOARD_BEGIN,
+  EDIT_BOARD_SUCCESS,
+  EDIT_BOARD_ERROR,
+  EDIT_BOARD_MODAL_TOGGLE,
 } from '../utils/actions';
 
 const boardReducer = (state, action) => {
+  /* MODALS */
+
   if (action.type === SIDE_CREATE_MODAL_TOGGLE) {
     return { ...state, sideBoardModalVisible: !state.sideBoardModalVisible };
   }
@@ -24,14 +34,27 @@ const boardReducer = (state, action) => {
     };
   }
 
+  if (action.type === EDIT_BOARD_MODAL_TOGGLE) {
+    return { ...state, editBoardVisible: !state.editBoardVisible };
+  }
+
+  if (action.type === DELETE_MODAL_TOGGLE) {
+    return { ...state, deleteModalVisible: !state.deleteModalVisible };
+  }
+
   if (action.type === CLOSE_MODAL) {
     return {
       ...state,
       createBoardVisible: false,
       sideBoardModalVisible: false,
+      deleteModalVisible: false,
+      editBoardVisible: false,
     };
   }
 
+  /* CRUD BOARDS */
+
+  /* CREATE */
   if (action.type === CREATE_BOARD_BEGIN) {
     return { ...state, isLoading: true };
   }
@@ -40,7 +63,6 @@ const boardReducer = (state, action) => {
     return {
       ...state,
       isLoading: false,
-      createBoardVisible: false,
     };
   }
 
@@ -48,22 +70,30 @@ const boardReducer = (state, action) => {
     return { ...state, isLoading: false };
   }
 
+  /* GET */
   if (action.type === GET_USER_BOARD_BEGIN) {
     return { ...state, isLoading: true };
   }
 
   if (action.type === GET_USER_BOARD_SUCCESS) {
+    const activeBoard = action.payload.boards.find(
+      (board) => board._id === action.payload.activeBoardId
+    );
+
     return {
       ...state,
       isLoading: false,
       boards: action.payload,
       activeBoardId: action.payload.activeBoardId,
+      activeBoard,
     };
   }
 
   if (action.type === GET_USER_BOARD_ERROR) {
     return { ...state, isLoading: false };
   }
+
+  /* GET ACTIVE BOARD */
 
   if (action.type === GET_USER_BOARD_COLUMN_SUCCESS) {
     const activeBoard = state.boards.boards.find(
@@ -75,6 +105,34 @@ const boardReducer = (state, action) => {
       activeBoardId: action.payload,
       activeBoard,
     };
+  }
+
+  /* DELETE BOARD */
+
+  if (action.type === DELETE_BOARD_BEGIN) {
+    return { ...state, isLoading: true };
+  }
+
+  if (action.type === DELETE_BOARD_SUCCESS) {
+    return { ...state, isLoading: false };
+  }
+
+  if (action.type === DELETE_BOARD_ERROR) {
+    return { ...state, isLoading: false };
+  }
+
+  /* EDIT BOARD */
+
+  if (action.type === EDIT_BOARD_BEGIN) {
+    return { ...state, isLoading: true };
+  }
+
+  if (action.type === EDIT_BOARD_SUCCESS) {
+    return { ...state, isLoading: false };
+  }
+
+  if (action.type === EDIT_BOARD_ERROR) {
+    return { ...state, isLoading: false };
   }
 
   throw new Error(`No Matching "${action.type}" - action type`);
