6b5e3af1b23f80deeb08ce070a4e39874955f30f
Anthony Maras <marasantonis@gmail.com>
2023-02-11 18:48:36 +0200

    set up spinner

---
+++ client/src/components/spinner/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PulseLoader } from 'react-spinners';
+
+const Spinner = () => {
+  return <PulseLoader color="#ffffff" size={5} />;
+};
+
+export default Spinner;
