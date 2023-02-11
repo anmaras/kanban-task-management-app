293d3d99c2435705e777146f7d40a1fd7ad39b6c
Anthony Maras <marasantonis@gmail.com>
2023-02-11 12:18:26 +0200

    set up get all boards controller

--- server/controllers/boardController.js
+++ server/controllers/boardController.js
@@ -4,7 +4,7 @@ import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
 
 const createBoard = async (req, res) => {
   const { name, columns } = req.body;
-  const columnsArray = columns.map((column) => ({ name: column }));
+  const columnsArray = columns.map((column) => ({ name: column.name }));
 
   if (!name || !columns) {
     throw new BadRequestError('Please provide all values');
@@ -16,11 +16,32 @@ const createBoard = async (req, res) => {
     userId: req.user.userId,
   });
 
-  res.status(StatusCodes.CREATED).json({ boards: [board] });
+  // const board = await Board.create({
+  //   activeBoard: name,
+  //   boards: [{ name, columns: columnsArray, userId: req.user.userId }],
+  // });
+
+  res.status(StatusCodes.CREATED).json(board);
 };
 
 const getAllBoards = async (req, res) => {
-  res.send('get all boards');
+  const boards = await Board.find({ userId: req.user.userId });
+
+  res.status(StatusCodes.OK).json({
+    boards,
+    totalBoards: boards.length,
+    activeBoardId: boards[boards.length - 1]._id,
+  });
 };
 
-export { createBoard, getAllBoards };
+const getBoardColumns = async (req, res) => {
+  const { id } = req.params;
+
+  const board = await Board.findOne({ _id: id });
+
+  res.status(StatusCodes.OK).json({
+    board,
+  });
+};
+
+export { createBoard, getAllBoards, getBoardColumns };
