311ba93dad5cb16ec509d1f348070a1fa27d7d1f
Anthony Maras <marasantonis@gmail.com>
2023-02-11 18:50:11 +0200

    add del and upd routes

--- server/routes/boardRoutes.js
+++ server/routes/boardRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 import {
   createBoard,
   getAllBoards,
-  getBoardColumns,
+  deleteBoard,
+  updateBoard,
 } from '../controllers/boardController.js';
 
 router.route('/create-board').post(createBoard);
 router.route('/get-user-boards').get(getAllBoards);
-router.route('/get-user-boards/:id').get(getBoardColumns);
+router.route('/get-user-boards/:id').delete(deleteBoard).patch(updateBoard);
 
 export default router;
