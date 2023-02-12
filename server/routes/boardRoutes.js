a15ddcbe7bc65df58f360b6e496074d32a734b96
Anthony Maras <marasantonis@gmail.com>
2023-02-13 00:15:52 +0200

    set up route for active board

--- server/routes/boardRoutes.js
+++ server/routes/boardRoutes.js
@@ -5,10 +5,12 @@ import {
   getAllBoards,
   deleteBoard,
   updateBoard,
+  selectActive,
 } from '../controllers/boardController.js';
 
 router.route('/create-board').post(createBoard);
 router.route('/get-user-boards').get(getAllBoards);
 router.route('/get-user-boards/:id').delete(deleteBoard).patch(updateBoard);
+router.route('/get-user-boards/active/:id').patch(selectActive);
 
 export default router;
