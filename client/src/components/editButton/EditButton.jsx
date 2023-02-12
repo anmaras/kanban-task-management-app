6fdf527a89aeb1feff56e0adbcebdd2675f0984f
Anthony Maras <marasantonis@gmail.com>
2023-02-13 00:11:28 +0200

    remove from animate presence the key

--- client/src/components/editButton/EditButton.jsx
+++ client/src/components/editButton/EditButton.jsx
@@ -24,7 +24,7 @@ const EditButton = ({ type }) => {
       >
         <Dots />
       </div>
-      <AnimatePresence key="edit-btn">
+      <AnimatePresence>
         {state && (
           <motion.div
             className={style['editButton__dropdown-container']}
