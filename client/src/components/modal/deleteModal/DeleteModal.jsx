559488e1bfadfd45ef8963c1c7c7bca155436234
Anthony Maras <marasantonis@gmail.com>
2023-02-13 00:13:11 +0200

    refactor delete modal

--- client/src/components/modal/deleteModal/DeleteModal.jsx
+++ client/src/components/modal/deleteModal/DeleteModal.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { useBoardContext } from '../../../context/boardsContext';
 import Spinner from '../../spinner/Spinner';
+import style from './DeleteModal.module.scss';
 
 const DeleteModal = ({ type }) => {
   const { activeBoard, deleteBoard, closeModal, isLoading } = useBoardContext();
   return (
-    <article>
-      <h2>Delete this board</h2>
-      <p>
-        {`Are you sure you want to delete the ${activeBoard?.name} board? This action
-        will remove all columns and tasks and cannot be reversed.`}
+    <article className={style.modal}>
+      <h2 className={[style['modal__title'], 'heading-L'].join(' ')}>
+        {type === 'boards' ? 'Delete this board?' : 'Delete this task?'}
+      </h2>
+      <p className={[style['modal__subtitle'], 'body-L-dark'].join(' ')}>
+        Are you sure you want to delete the <b>{activeBoard?.name}</b> board?
+        This action will remove all columns and tasks and cannot be reversed.
       </p>
-      <button className="button" onClick={deleteBoard}>
-        {isLoading ? <Spinner /> : 'Delete'}
-      </button>
-      <button className="button" onClick={closeModal}>
-        Cancel
-      </button>
+      <div className={style['modal__btns-container']}>
+        <button
+          className="button button--secondary--delete"
+          onClick={deleteBoard}
+        >
+          {isLoading ? <Spinner /> : 'Delete'}
+        </button>
+        <button className="button button--secondary" onClick={closeModal}>
+          Cancel
+        </button>
+      </div>
     </article>
   );
 };
