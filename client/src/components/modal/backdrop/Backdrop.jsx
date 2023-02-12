b23fa65d700d7bad5ab8c6432caa9f87286945eb
Anthony Maras <marasantonis@gmail.com>
2023-02-13 00:12:10 +0200

    add animation at backdrop

--- client/src/components/modal/backdrop/Backdrop.jsx
+++ client/src/components/modal/backdrop/Backdrop.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useBoardContext } from '../../../context/boardsContext';
 import style from './Backdrop.module.scss';
+import { motion } from 'framer-motion';
 
 const Backdrop = ({ children }) => {
   const { closeModal } = useBoardContext();
   return (
-    <div
+    <motion.div
       className={style.backdrop}
+      key="modal"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.2 }}
       onClick={(e) => {
         if (e.target === e.currentTarget) {
           //need to stop the bubble so modals to work
@@ -16,7 +22,7 @@ const Backdrop = ({ children }) => {
       }}
     >
       {children}
-    </div>
+    </motion.div>
   );
 };
 
