31e00960c8688ffaa7ef2961f19da5e25c413d1f
Anthony Maras <marasantonis@gmail.com>
2023-02-13 00:12:38 +0200

    refactor board moral

--- client/src/components/modal/boardModals/BoardModals.jsx
+++ client/src/components/modal/boardModals/BoardModals.jsx
@@ -9,6 +9,8 @@ import {
 import { useBoardContext } from '../../../context/boardsContext';
 import * as yup from 'yup';
 import Spinner from '../../spinner/Spinner';
+import style from './BoardModals.module.scss';
+import { ReactComponent as DeleteIcon } from '../../../assets/icon-cross.svg';
 
 const initialValues = {
   name: '',
@@ -16,10 +18,10 @@ const initialValues = {
 };
 
 const validationCreateBoard = yup.object({
-  name: yup.string().trim().required('Required'),
+  name: yup.string().trim().required('Required!'),
   columns: yup.array().of(
     yup.object({
-      name: yup.string().required('Required'),
+      name: yup.string().required('Required!'),
     })
   ),
 });
@@ -27,62 +29,118 @@ const validationCreateBoard = yup.object({
 const BoardModals = ({ type }) => {
   const { createBoard, isLoading, activeBoard, editBoard } = useBoardContext();
 
-  const valueEdit = {
+  const valuesForEdit = {
     name: activeBoard?.name,
     columns: activeBoard?.columns,
   };
 
   return (
-    <article>
-      <h2>{type === 'create' ? 'Add New Board' : 'Edit Board'}</h2>
+    <article className={style.modal}>
+      <h2 className={[style['modal__title'], 'heading-L'].join(' ')}>
+        {type === 'create' ? 'Add New Board' : 'Edit Board'}
+      </h2>
+
       <Formik
-        initialValues={type === 'create' ? initialValues : valueEdit}
+        initialValues={type === 'create' ? initialValues : valuesForEdit}
         validationSchema={validationCreateBoard}
         onSubmit={type === 'create' ? createBoard : editBoard}
       >
         {({ touched, errors, values }) => (
-          <FormikForm>
-            <div className="form-control">
-              <label htmlFor="name">Board Name</label>
-              <Field type="text" name="name" id="name" />
-              <div>
+          <FormikForm className={style['modal__form']}>
+            <div className={style['modal__controller']}>
+              <label
+                className={[style['modal__label'], 'body-M '].join(' ')}
+                htmlFor="name"
+              >
+                Board Name
+              </label>
+              <Field
+                className={[
+                  style[
+                    touched.name && errors.name
+                      ? 'modal__input--error'
+                      : 'modal__input'
+                  ],
+                  'body-L-dark',
+                ].join(' ')}
+                type="text"
+                name="name"
+                id="name"
+                placeholder="e.g Web Design"
+              />
+              <div
+                className={[
+                  style['modal__error-container'],
+                  'body-L-dark--error',
+                ].join(' ')}
+              >
                 <ErrorMessage name="name" component="div" />
               </div>
             </div>
+
             <FieldArray
               name="columns"
               render={(arrayHelpers) => (
-                <div>
-                  {values.columns.map((column, index) => {
-                    return (
-                      <div key={index}>
-                        <Field name={`columns[${index}].name`} />
-                        <ErrorMessage
-                          name={`columns[${index}].name`}
-                          component="div"
-                        />
-                        {values.columns.length > 1 ? (
-                          <button
-                            type="button"
-                            onClick={() => arrayHelpers.remove(index)}
-                          >
-                            delete
-                          </button>
-                        ) : null}
-                      </div>
-                    );
-                  })}
+                <section className={style['modal__list-section']}>
+                  <h3 className="body-M">Board Columns</h3>
+                  <ul className={style['modal__list-container']}>
+                    {values.columns.map((column, index) => {
+                      //check if there is any error at array fields
+                      let error = errors?.columns?.[index];
+                      let touch = touched?.columns?.[index];
+
+                      return (
+                        <li className={style['modal__list-item']} key={index}>
+                          <div className={style['modal__list-item-controller']}>
+                            <Field
+                              name={`columns[${index}].name`}
+                              placeholder="Column Name"
+                              className={[
+                                style[
+                                  error && touch
+                                    ? 'modal__list-item-input--error'
+                                    : 'modal__list-item-input'
+                                ],
+                                'body-L-dark',
+                              ].join(' ')}
+                            />
+                            <div
+                              className={[
+                                style['modal__list-item-error-container'],
+                                'body-L-dark--error',
+                              ].join(' ')}
+                            >
+                              <ErrorMessage
+                                name={`columns[${index}].name`}
+                                component="div"
+                              />
+                            </div>
+                          </div>
+                          {values.columns.length > 1 ? (
+                            <button
+                              className="button--del-icon"
+                              type="button"
+                              onClick={() => arrayHelpers.remove(index)}
+                            >
+                              <DeleteIcon />
+                            </button>
+                          ) : null}
+                        </li>
+                      );
+                    })}
+                  </ul>
                   <button
+                    className="button button--secondary"
                     type="button"
                     onClick={() => arrayHelpers.push({ name: '' })}
                   >
-                    add
+                    add new column
                   </button>
-                </div>
+                </section>
               )}
             />
 
-            <button type="submit">
+            <button className="button button--primary-S" type="submit">
               {isLoading ? (
                 <Spinner />
               ) : type === 'create' ? (
