2bbdc592ffa73bc79485eb380b312055d034914c
Anthony Maras <marasantonis@gmail.com>
2023-02-14 16:38:38 +0200

    update index

--- client/src/components/modal/index.jsx
+++ client/src/components/modal/index.jsx
@@ -1,5 +1,6 @@
 import BoardModals from './boardModals/BoardModals';
 import Backdrop from './backdrop/Backdrop';
 import DeleteModal from './deleteModal/DeleteModal';
+import TaskModal from './taskModal/TaskModal';
 
-export { DeleteModal, BoardModals, Backdrop };
+export { DeleteModal, BoardModals, Backdrop, TaskModal };
