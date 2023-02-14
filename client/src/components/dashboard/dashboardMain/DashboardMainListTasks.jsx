f066a91f6782bfb4ca7ad99c2734cca751e91c19
Anthony Maras <marasantonis@gmail.com>
2023-02-14 14:09:00 +0200

    fixed reducer to update correctly the state board

--- client/src/components/dashboard/dashboardMain/DashboardMainListTasks.jsx
+++ client/src/components/dashboard/dashboardMain/DashboardMainListTasks.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './DashboardMainListTask.module.scss';
 
 const DashboardMainListTasks = ({ tasks }) => {
   return (
-    <ul>
+    <ul className={style.tasksList}>
       {tasks.map((task) => {
         return <li key={task.title}>{task.title}</li>;
       })}
