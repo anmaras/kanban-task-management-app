40945a68d6b1670e2cc9d8ae2845f1de98ef4ec9
Anthony Maras <marasantonis@gmail.com>
2023-02-14 14:10:35 +0200

    refactor the routes to seperate the boards and columns

--- server/routes/boardRoutes.js
+++ server/routes/boardRoutes.js
@@ -6,11 +6,19 @@ import {
   deleteBoard,
   updateBoard,
   selectActive,
+  createBoardTask,
 } from '../controllers/boardController.js';
 
 router.route('/create-board').post(createBoard);
 router.route('/get-user-boards').get(getAllBoards);
-router.route('/get-user-boards/:id').delete(deleteBoard).patch(updateBoard);
-router.route('/get-user-boards/active/:id').patch(selectActive);
+router
+  .route('/get-user-boards/board/:id')
+  .delete(deleteBoard)
+  .patch(updateBoard);
+router
+  .route('/get-user-boards/board/:id/column/:columnId')
+  .post(createBoardTask);
+
+router.route('/get-user-boards/active/board/:id').patch(selectActive);
 
 export default router;
