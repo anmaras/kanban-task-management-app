322a34555ad802bb4bc9637ead8ff864456727c5
Anthony Maras <marasantonis@gmail.com>
2023-02-15 20:01:24 +0200

    initial setup

--- client/src/components/dashboard/dashboardMain/DashboardMainListTasks.jsx
+++ client/src/components/dashboard/dashboardMain/DashboardMainListTasks.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import style from './DashboardMainListTask.module.scss';
+import { useBoardContext } from '../../../context/boardsContext';
 
 const DashboardMainListTasks = ({ tasks }) => {
+  const { handleViewTaskModal, getCurrentTask } = useBoardContext();
+
   return (
-    <ul className={style.tasksList}>
+    <ul
+      className={
+        tasks.length > 0 ? style['tasksList'] : style['tasksList--empty']
+      }
+    >
       {tasks.map((task) => {
-        return <li key={task.title}>{task.title}</li>;
+        const { title, _id, subtasks } = task;
+        const isCompleted = subtasks.filter((subtask) => subtask.isCompleted);
+
+        return (
+          <li
+            className={style['tasksList__item']}
+            key={_id}
+            onClick={() => {
+              getCurrentTask(task);
+              handleViewTaskModal();
+            }}
+          >
+            <h3 className={[style['tasksList__title'], 'heading-M'].join(' ')}>
+              {title}
+            </h3>
+            <p
+              className={[style['tasksList__subtitle'], 'body-M '].join(' ')}
+            >{`${isCompleted.length} of ${subtasks.length} subtasks`}</p>
+          </li>
+        );
       })}
     </ul>
   );
