0da1a8933cd6f544ab8009e62c9e7c8f55545e2a
Anthony Maras <marasantonis@gmail.com>
2023-02-20 12:32:16 +0200

    add in spinner an extra return

--- client/src/components/spinner/Spinner.jsx
+++ client/src/components/spinner/Spinner.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { PulseLoader } from 'react-spinners';
+import { PulseLoader, PropagateLoader } from 'react-spinners';
 
-const Spinner = () => {
+const Spinner = ({ type }) => {
+  if (type === 'big') {
+    return <PropagateLoader color="#ffffff" size={15} />;
+  }
+
   return <PulseLoader color="#ffffff" size={5} />;
 };
 
