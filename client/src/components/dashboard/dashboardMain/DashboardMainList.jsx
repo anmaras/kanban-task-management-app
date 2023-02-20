8bcb80e727f0c94a7b59ae4d424737283116d5a7
Anthony Maras <marasantonis@gmail.com>
2023-02-20 22:43:06 +0200

    adding dnd functionality and components

--- client/src/components/dashboard/dashboardMain/DashboardMainList.jsx
+++ client/src/components/dashboard/dashboardMain/DashboardMainList.jsx
@@ -1,40 +1,89 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { useBoardContext } from '../../../context/boardsContext';
 import style from './DashboardMainList.module.scss';
 import DashboardMainListTasks from './DashboardMainListTasks';
+import { DragDropContext } from 'react-beautiful-dnd';
 
-const DashboardMainList = () => {
+const DashboardMainList = ({ isVisible }) => {
   const { activeBoard, handleAddColumnModal } = useBoardContext();
+  const [draggableData, setDraggableData] = useState(activeBoard);
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const sourceCol = draggableData?.columns?.find(
+      (col) => col._id === source.droppableId
+    );
+    // const destinationCol = draggableData?.columns?.find(
+    //   (col) => col._id === destination.droppableId
+    // );
+
+    const task = sourceCol?.tasks[source.index];
+
+    sourceCol?.tasks?.splice(source.index, 1);
+    sourceCol?.tasks?.splice(destination.index, 0, task);
+
+    const newCol = {
+      ...sourceCol,
+      tasks: sourceCol.tasks,
+    };
+
+    const newDraggableData = {
+      ...draggableData,
+      columns: draggableData.columns.map((col) =>
+        col._id === source.droppableId ? newCol : col
+      ),
+    };
+
+    setDraggableData(newDraggableData);
+  };
+
   return (
-    <ul className={style.list}>
-      {activeBoard?.columns?.map((column) => {
-        return (
-          <li className={style['list__item']} key={column._id}>
-            <div className={style['list__title-container']}>
-              <div className={style['list__color']}></div>
-              <div className={[style['list__name'], 'heading-S'].join(' ')}>
-                {column?.name} ({column?.tasks?.length})
+    <DragDropContext onDragEnd={onDragEnd}>
+      <ul className={style.list}>
+        {activeBoard?.columns?.map((column) => {
+          return (
+            <li className={style['list__item']} key={column._id}>
+              <div className={style['list__title-container']}>
+                <div className={style['list__color']}></div>
+                <div className={[style['list__name'], 'heading-S'].join(' ')}>
+                  {column?.name} ({column?.tasks?.length})
+                </div>
               </div>
-            </div>
-            <DashboardMainListTasks tasks={column?.tasks} />
-          </li>
-        );
-      })}
-      <li className={style['list__item']}>
-        <div className={style['list__title-container']}>
-          <div className={style['list__color']}></div>
-        </div>
-        <div className={style['list__btn-container']}>
-          <button
-            className="button heading-XL button-addColumn"
-            onClick={handleAddColumnModal}
-          >
-            add new column
-          </button>
-        </div>
-      </li>
-    </ul>
+              <DashboardMainListTasks
+                tasks={column?.tasks}
+                columnId={column?._id}
+                isVisible={isVisible}
+              />
+            </li>
+          );
+        })}
+        <li className={style['list__item']}>
+          <div className={style['list__title-container']}>
+            <div className={style['list__color']}></div>
+          </div>
+          <div className={style['list__btn-container']}>
+            <button
+              className="button heading-XL button-addColumn"
+              onClick={handleAddColumnModal}
+            >
+              add new column
+            </button>
+          </div>
+        </li>
+      </ul>
+    </DragDropContext>
   );
 };
 
--- client/src/components/dashboard/dashboardMain/DashboardMainListTasks.jsx
+++ client/src/components/dashboard/dashboardMain/DashboardMainListTasks.jsx
@@ -1,40 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import style from './DashboardMainListTask.module.scss';
 import { useBoardContext } from '../../../context/boardsContext';
+import { Draggable } from 'react-beautiful-dnd';
+import { StrictModeDroppable as Droppable } from '../../../utils/StrictModeDroppable';
 
-const DashboardMainListTasks = ({ tasks }) => {
+const DashboardMainListTasks = ({ tasks, columnId, isVisible }) => {
   const { handleViewTaskModal, getCurrentTask, activeBoard, activeColumn } =
     useBoardContext();
 
   return (
-    <ul
-      className={
-        tasks.length > 0 ? style['tasksList'] : style['tasksList--empty']
-      }
-    >
-      {tasks.map((task) => {
-        const { title, _id, subtasks } = task;
-        const isCompleted = subtasks.filter((subtask) => subtask.isCompleted);
+    <Droppable droppableId={columnId}>
+      {(provided) => (
+        <ul
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          className={
+            tasks.length > 0 ? style['tasksList'] : style['tasksList--empty']
+          }
+        >
+          {tasks.map((task, index) => {
+            const { title, _id, subtasks } = task;
+            const isCompleted = subtasks.filter(
+              (subtask) => subtask.isCompleted
+            );
 
-        return (
-          <li
-            className={[style['tasksList__item'], 'card'].join(' ')}
-            key={_id}
-            onClick={() => {
-              getCurrentTask(task);
-              handleViewTaskModal();
-            }}
-          >
-            <h3 className={[style['tasksList__title'], 'heading-M'].join(' ')}>
-              {title}
-            </h3>
-            <p
-              className={[style['tasksList__subtitle'], 'body-M '].join(' ')}
-            >{`${isCompleted.length} of ${subtasks.length} subtasks`}</p>
-          </li>
-        );
-      })}
-    </ul>
+            return (
+              <Draggable draggableId={_id} index={index} key={_id}>
+                {(provided) => (
+                  <li
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    className={[style['tasksList__item'], 'card'].join(' ')}
+                    onClick={() => {
+                      getCurrentTask(task);
+                      handleViewTaskModal();
+                    }}
+                  >
+                    <h3
+                      className={[style['tasksList__title'], 'heading-M'].join(
+                        ' '
+                      )}
+                    >
+                      {title}
+                    </h3>
+                    <p
+                      className={[style['tasksList__subtitle'], 'body-M '].join(
+                        ' '
+                      )}
+                    >{`${isCompleted.length} of ${subtasks.length} subtasks`}</p>
+                  </li>
+                )}
+              </Draggable>
+            );
+          })}
+          {provided.placeholder}
+        </ul>
+      )}
+    </Droppable>
   );
 };
 
