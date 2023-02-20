3d06257be3f62b52f8e77f982d4015a996511b3c
Anthony Maras <marasantonis@gmail.com>
2023-02-20 12:31:37 +0200

    add loader

--- client/src/components/layouts/dashboardLayout/DashboardLayout.jsx
+++ client/src/components/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import style from './DashboardLayout.module.scss';
-import { DashboardHeader } from '../../index';
+import { DashboardHeader, Loader } from '../../index';
 import ScrollContainer from 'react-indiana-drag-scroll';
+import { useBoardContext } from '../../../context/boardsContext';
 
 const DashboardLayout = ({ children }) => {
+  const { fetchDataLoading } = useBoardContext();
+
+  if (fetchDataLoading) {
+    return <Loader />;
+  }
+
   return (
     <section className={style.layout}>
       <DashboardHeader />
