0617c26177626045581cac826f68cd806c9b1781
Anthony Maras <marasantonis@gmail.com>
2023-02-20 12:31:46 +0200

    set up loader

---
+++ client/src/components/loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Loader.module.scss';
+import Spinner from '../spinner/Spinner';
+
+const Loader = () => {
+  return (
+    <div className={style.container}>
+      <Spinner type="big" />
+    </div>
+  );
+};
+
+export default Loader;
