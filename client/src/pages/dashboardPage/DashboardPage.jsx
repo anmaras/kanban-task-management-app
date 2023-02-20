8535a20b5d6527644afd9678e479a39992f3522b
Anthony Maras <marasantonis@gmail.com>
2023-02-20 12:33:34 +0200

    add create portal

--- client/src/pages/dashboardPage/DashboardPage.jsx
+++ client/src/pages/dashboardPage/DashboardPage.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { useUserContext } from '../../context/userContext';
+import { createPortal } from 'react-dom';
 import {
   DashboardLayout,
   DashboardMain,
   DashboardSide,
+  PortalComponents,
 } from '../../components';
 import { ReactComponent as Eye } from '../../assets/icon-show-sidebar.svg';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const DashboardPage = () => {
-  const { logoutUser, user } = useUserContext();
   const [isVisible, setIsVisible] = useState(true);
 
   const setVisibility = () => {
@@ -17,29 +17,27 @@ const DashboardPage = () => {
   };
 
   return (
-    <DashboardLayout>
-      <DashboardSide isVisible={isVisible} setVisibility={setVisibility} />
-      <DashboardMain isVisible={isVisible} />
-      {/* <div>
-        <h1>Dashboard</h1>
-        <h2>{user.name}</h2>
-        <button onClick={logoutUser}>Logout</button>
-      </div> */}
-      <AnimatePresence>
-        {!isVisible && (
-          <motion.button
-            onClick={setVisibility}
-            className="button-showSide"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.1 }}
-          >
-            <Eye />
-          </motion.button>
-        )}
-      </AnimatePresence>
-    </DashboardLayout>
+    <>
+      {createPortal(<PortalComponents />, document.body)}
+      <DashboardLayout>
+        <DashboardSide isVisible={isVisible} setVisibility={setVisibility} />
+        <DashboardMain isVisible={isVisible} />
+        <AnimatePresence>
+          {!isVisible && (
+            <motion.button
+              onClick={setVisibility}
+              className="button-showSide"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.1 }}
+            >
+              <Eye />
+            </motion.button>
+          )}
+        </AnimatePresence>
+      </DashboardLayout>
+    </>
   );
 };
 
