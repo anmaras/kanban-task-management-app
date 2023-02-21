5a65b89b0b666a1cc36a4d1ecced7f13578dcc9f
Anthony Maras <marasantonis@gmail.com>
2023-02-21 14:37:14 +0200

    finalize dnd drag

--- client/src/components/dashboard/dashboardMain/DashboardMainList.jsx
+++ client/src/components/dashboard/dashboardMain/DashboardMainList.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useBoardContext } from '../../../context/boardsContext';
 import style from './DashboardMainList.module.scss';
 import DashboardMainListTasks from './DashboardMainListTasks';
 import { DragDropContext } from 'react-beautiful-dnd';
 
-const DashboardMainList = ({ isVisible }) => {
-  const { activeBoard, handleAddColumnModal } = useBoardContext();
+const DashboardMainList = () => {
+  const { activeBoard, handleAddColumnModal, dndTask } = useBoardContext();
   const [draggableData, setDraggableData] = useState(activeBoard);
 
   const onDragEnd = (result) => {
-    const { destination, source, draggableId } = result;
+    const { destination, source } = result;
 
     if (!destination) {
       return;
@@ -25,34 +25,70 @@ const DashboardMainList = ({ isVisible }) => {
     const sourceCol = draggableData?.columns?.find(
       (col) => col._id === source.droppableId
     );
-    // const destinationCol = draggableData?.columns?.find(
-    //   (col) => col._id === destination.droppableId
-    // );
-
+    const destinationCol = draggableData?.columns?.find(
+      (col) => col._id === destination.droppableId
+    );
     const task = sourceCol?.tasks[source.index];
 
-    sourceCol?.tasks?.splice(source.index, 1);
-    sourceCol?.tasks?.splice(destination.index, 0, task);
+    if (sourceCol._id === destinationCol._id) {
+      sourceCol?.tasks?.splice(source.index, 1);
+      sourceCol?.tasks?.splice(destination.index, 0, task);
 
-    const newCol = {
+      const newSourceCol = {
+        ...sourceCol,
+        tasks: sourceCol.tasks,
+      };
+
+      const newDraggableData = {
+        ...draggableData,
+        columns: draggableData.columns.map((col) =>
+          col._id === source.droppableId ? newSourceCol : col
+        ),
+      };
+
+      setDraggableData(newDraggableData);
+      dndTask(result);
+      return;
+    }
+
+    sourceCol?.tasks?.splice(source.index, 1);
+
+    const newSourceCol = {
       ...sourceCol,
-      tasks: sourceCol.tasks,
+      tasks: sourceCol?.tasks,
+    };
+
+    destinationCol?.tasks?.splice(destination.index, 0, task);
+
+    const newDestCol = {
+      ...destinationCol,
+      tasks: destinationCol?.tasks,
     };
 
     const newDraggableData = {
       ...draggableData,
       columns: draggableData.columns.map((col) =>
-        col._id === source.droppableId ? newCol : col
+        col._id === source.droppableId
+          ? newSourceCol
+          : col._id === destination.droppableId
+          ? newDestCol
+          : col
       ),
     };
 
     setDraggableData(newDraggableData);
+    dndTask(result);
+    return;
   };
+
+  useEffect(() => {
+    setDraggableData(activeBoard);
+  }, [activeBoard]);
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <ul className={style.list}>
-        {activeBoard?.columns?.map((column) => {
+        {draggableData?.columns?.map((column) => {
           return (
             <li className={style['list__item']} key={column._id}>
               <div className={style['list__title-container']}>
@@ -64,7 +100,6 @@ const DashboardMainList = ({ isVisible }) => {
               <DashboardMainListTasks
                 tasks={column?.tasks}
                 columnId={column?._id}
-                isVisible={isVisible}
               />
             </li>
           );
