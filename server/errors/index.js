41576cc3f0fca053e723329d563560ad143c7d2a
Anthony Maras <marasantonis@gmail.com>
2023-02-21 14:35:34 +0200

    update error index

--- server/errors/index.js
+++ server/errors/index.js
@@ -1,5 +1,6 @@
 import BadRequestError from './bad-request.js';
 import NotFoundError from './not-found.js';
 import UnauthenticatedError from './unauthenticated.js';
+import NoContent from './no-content.js';
 
-export { BadRequestError, NotFoundError, UnauthenticatedError };
+export { BadRequestError, NotFoundError, UnauthenticatedError, NoContent };
