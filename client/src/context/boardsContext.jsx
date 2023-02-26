02bfd73ee7c4b3744fcf8b33447d96d478fc35a0
Anthony Maras <marasantonis@gmail.com>
2023-02-26 19:14:48 +0200

    board context refactoring and added modals context

--- client/src/context/boardsContext.jsx
+++ client/src/context/boardsContext.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useReducer, useEffect } from 'react';
 import { useUserContext } from './userContext';
+import { useModalContext } from './modalsContext';
 import reducer from '../reducers/boardsReducer';
 import axios from 'axios';
 
@@ -7,10 +8,6 @@ import {
   CREATE_BOARD_BEGIN,
   CREATE_BOARD_SUCCESS,
   CREATE_BOARD_ERROR,
-  SIDE_CREATE_MODAL_TOGGLE,
-  CREATE_NEW_BOARD_MODAL_TOGGLE,
-  DELETE_MODAL_TOGGLE,
-  CLOSE_MODAL,
   GET_USER_BOARD_BEGIN,
   GET_USER_BOARD_SUCCESS,
   GET_USER_BOARD_ERROR,
@@ -24,18 +21,12 @@ import {
   CREATE_COLUMN_TASK_BEGIN,
   CREATE_COLUMN_TASK_SUCCESS,
   CREATE_COLUMN_TASK_ERROR,
-  EDIT_BOARD_MODAL_TOGGLE,
-  ADD_COLUMN_MODAL_TOGGLE,
-  ADD_TASK_MODAL_TOGGLE,
-  VIEW_TASK_MODAL_TOGGLE,
   GET_CURRENT_TASK,
   EDIT_SUBTASK,
   MOVE_TASK,
   DELETE_TASK_BEGIN,
   DELETE_TASK_SUCCESS,
   DELETE_TASK_ERROR,
-  DELETE_TASK_MODAL_TOGGLE,
-  UPDATE_TASK_MODAL_TOGGLE,
   EDIT_TASK_BEGIN,
   EDIT_TASK_SUCCESS,
   EDIT_TASK_ERROR,
@@ -49,15 +40,6 @@ export const initialState = {
   boards: [],
   totalBoards: 0,
   fetchDataLoading: false,
-  createBoardVisible: false,
-  sideBoardModalVisible: false,
-  deleteModalVisible: false,
-  editBoardVisible: false,
-  addColumnModalVisible: false,
-  addTaskModalVisible: false,
-  viewTaskModalVisible: false,
-  deleteTaskModalVisible: false,
-  updateTaskModalVisible: false,
   activeBoardId: '',
   activeBoard: {},
   task: {},
@@ -67,291 +49,187 @@ export const initialState = {
 const BoardContext = React.createContext();
 
 export const BoardProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
   const { token, user, logoutUser } = useUserContext();
+  const { closeModals } = useModalContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { activeBoardId, activeColumn, task } = state;
 
-  const handleCreateBoardModal = () => {
-    dispatch({ type: CREATE_NEW_BOARD_MODAL_TOGGLE });
+  const axiosInstance = axios.create({
+    baseURL: '/api/v1/boards',
+  });
+
+  /* INCLUDE TOKEN ON HEADER FOR ALL REQUESTS */
+  axiosInstance.interceptors.request.use((config) => {
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response.status === 401) {
+        logoutUser();
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  /* GET ALL BOARDS */
+  const getUserBoards = async () => {
+    dispatch({ type: GET_USER_BOARD_BEGIN });
+    try {
+      const { data } = await axiosInstance.get(`/user-boards`);
+      dispatch({ type: GET_USER_BOARD_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({ type: GET_USER_BOARD_ERROR });
+    }
   };
 
-  const handleEditBoardModal = () => {
-    dispatch({ type: EDIT_BOARD_MODAL_TOGGLE });
-  };
-
-  const handleSideBoardModal = () => {
-    dispatch({ type: SIDE_CREATE_MODAL_TOGGLE });
-  };
-
-  const handleDeleteBoardModal = () => {
-    dispatch({ type: DELETE_MODAL_TOGGLE });
-  };
-
-  const handleAddColumnModal = () => {
-    dispatch({ type: ADD_COLUMN_MODAL_TOGGLE });
-  };
-
-  const handleAddTaskModal = () => {
-    dispatch({ type: ADD_TASK_MODAL_TOGGLE });
-  };
-
-  const handleViewTaskModal = () => {
-    dispatch({ type: VIEW_TASK_MODAL_TOGGLE });
-  };
-
-  const handleDeleteTaskModal = () => {
-    dispatch({ type: DELETE_TASK_MODAL_TOGGLE });
-  };
-
-  const handleEditTaskModal = () => {
-    dispatch({ type: UPDATE_TASK_MODAL_TOGGLE });
-  };
-
-  const closeModal = () => {
-    dispatch({ type: CLOSE_MODAL });
-  };
-
+  /* CREATE BOARD */
   const createBoard = async (values) => {
     dispatch({ type: CREATE_BOARD_BEGIN });
 
     try {
-      const { data } = await axios.post('/api/v1/boards/create-board', values, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const { data } = await axiosInstance.post(`/create-board`, values);
       dispatch({ type: CREATE_BOARD_SUCCESS, payload: data });
-      closeModal();
+      closeModals();
     } catch (error) {
       dispatch({ type: CREATE_BOARD_ERROR });
-      if (error.request.status === 401) {
-        logoutUser();
-      }
     }
   };
 
-  const getUserBoards = async () => {
-    dispatch({ type: GET_USER_BOARD_BEGIN });
+  /* SET ACTIVE BOARD */
+  const setActiveBoardId = async (id) => {
     try {
-      const { data } = await axios.get('/api/v1/boards/get-user-boards', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+      const { data } = await axiosInstance.patch(`/active/board/${id}`, {
+        isActive: true,
       });
-      dispatch({ type: GET_USER_BOARD_SUCCESS, payload: data });
-    } catch (error) {
-      dispatch({ type: GET_USER_BOARD_ERROR });
-      if (error.request.status === 401) {
-        logoutUser();
-      }
-    }
-  };
-
-  const getActiveBoardId = async (activeBoardId) => {
-    try {
-      const { data } = await axios.patch(
-        `/api/v1/boards/get-user-boards/active/board/${activeBoardId}`,
-        { isActive: true },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
       dispatch({ type: GET_USER_BOARD_COLUMN_SUCCESS, payload: data });
     } catch (error) {
-      if (error.request.status === 401) {
-        logoutUser();
-      }
+      console.log(error);
     }
   };
 
+  /* DELETE BOARD */
   const deleteBoard = async () => {
     dispatch({ type: DELETE_BOARD_BEGIN });
     try {
-      const { data } = await axios.delete(
-        `/api/v1/boards/get-user-boards/board/${state.activeBoardId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      const { data } = await axiosInstance.delete(`/board/${activeBoardId}`);
       dispatch({ type: DELETE_BOARD_SUCCESS, payload: data });
-      closeModal();
+      closeModals();
     } catch (error) {
       dispatch({ type: DELETE_BOARD_ERROR });
-      if (error.request.status === 401) {
-        logoutUser();
-      }
     }
   };
 
+  /* EDIT BOARD */
   const editBoard = async (values) => {
     dispatch({ type: EDIT_BOARD_BEGIN });
     try {
-      const { data } = await axios.patch(
-        `/api/v1/boards/get-user-boards/board/${state.activeBoardId}`,
-        values,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
+      const { data } = await axiosInstance.patch(
+        `board/${activeBoardId}`,
+        values
       );
       dispatch({ type: EDIT_BOARD_SUCCESS, payload: data });
-      closeModal();
+      closeModals();
     } catch (error) {
       dispatch({ type: EDIT_BOARD_ERROR });
-      if (error.request.status === 401) {
-        logoutUser();
-      }
     }
   };
 
+  /* CREATE TASK */
   const addNewTask = async (values) => {
     const { columnId } = values;
 
     dispatch({ type: CREATE_COLUMN_TASK_BEGIN });
     try {
-      const { data } = await axios.post(
-        `/api/v1/boards/get-user-boards/board/${state.activeBoardId}/column/${columnId}`,
-        values,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
+      const { data } = await axiosInstance.post(
+        `/board/${activeBoardId}/column/${columnId}`,
+        values
       );
       dispatch({
         type: CREATE_COLUMN_TASK_SUCCESS,
         payload: data,
       });
-      closeModal();
+      closeModals();
     } catch (error) {
       dispatch({ type: CREATE_COLUMN_TASK_ERROR });
-      if (error.request.status === 401) {
-        logoutUser();
-      }
     }
   };
 
+  /* DELETE TASK */
+  const deleteTask = async () => {
+    dispatch({ type: DELETE_TASK_BEGIN });
+    try {
+      const { data } = await axiosInstance.delete(
+        `/board/${activeBoardId}/column/${activeColumn._id}/task/${task._id}`
+      );
+      dispatch({ type: DELETE_TASK_SUCCESS, payload: data });
+      closeModals();
+    } catch (error) {
+      dispatch({ type: DELETE_TASK_ERROR });
+    }
+  };
+
+  /* EDIT TASK */
+  const editTask = async (values) => {
+    dispatch({ type: EDIT_TASK_BEGIN });
+    try {
+      const { data } = await axiosInstance.patch(
+        `/board/${activeBoardId}/column/${activeColumn._id}/task/${task._id}`,
+        values
+      );
+      dispatch({ type: EDIT_TASK_SUCCESS, payload: data });
+      closeModals();
+    } catch (error) {
+      dispatch({ type: EDIT_TASK_ERROR });
+    }
+  };
+
+  /* GET CURRENT TASK */
   const getCurrentTask = (task) => {
     dispatch({ type: GET_CURRENT_TASK, payload: task });
   };
 
+  /* EDIT SUBTASKS */
   const editSubTaskCheckBox = async (subtaskId, columnId) => {
     try {
-      const { data } = await axios.patch(
-        `/api/v1/boards/board/${state.activeBoardId}/column/${columnId}/task/${state.task._id}/subtask/${subtaskId}`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
+      const { data } = await axiosInstance.patch(
+        `/board/${state.activeBoardId}/column/${columnId}/task/${state.task._id}/subtask/${subtaskId}`,
+        {}
       );
       dispatch({ type: EDIT_SUBTASK, payload: data });
     } catch (error) {
-      if (error.request.status === 401) {
-        logoutUser();
-      }
+      console.log(error);
     }
   };
 
+  /* MOVE TASK */
   const moveTasks = async (from, columnId) => {
     try {
-      const { data } = await axios.patch(
-        `/api/v1/boards/board/${state.activeBoardId}/column/${from}/task/${state.task._id}/move`,
-        { activeTask: state.task, toId: columnId },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
+      const { data } = await axiosInstance.patch(
+        `/board/${activeBoardId}/column/${from}/task/${task._id}/move`,
+        { activeTask: task, toId: columnId }
       );
       dispatch({ type: MOVE_TASK, payload: data });
     } catch (error) {
-      if (error.request.status === 401) {
-        logoutUser();
-      }
+      console.log(error);
     }
   };
 
-  const deleteTask = async () => {
-    const { activeBoardId, activeColumn, task } = state;
-
-    dispatch({ type: DELETE_TASK_BEGIN });
-    try {
-      const { data } = await axios.delete(
-        `api/v1/boards/board/${activeBoardId}/column/${activeColumn._id}/task/${task._id}/delete`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      dispatch({ type: DELETE_TASK_SUCCESS, payload: data });
-      closeModal();
-    } catch (error) {
-      dispatch({ type: DELETE_TASK_ERROR });
-      if (error.request.status === 401) {
-        logoutUser();
-      }
-    }
-  };
-
-  const editTask = async (values) => {
-    dispatch({ type: EDIT_TASK_BEGIN });
-    try {
-      const { data } = await axios.patch(
-        `api/v1/boards/board/${state.activeBoardId}/column/${state.activeColumn._id}/task/${state.task._id}/edit`,
-        values,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      dispatch({ type: EDIT_TASK_SUCCESS, payload: data });
-      closeModal();
-    } catch (error) {
-      dispatch({ type: EDIT_TASK_ERROR });
-      if (error.request.status === 401) {
-        logoutUser();
-      }
-    }
-  };
-
+  /* DRAG AND DROP TASKS */
   const dndTask = async (values) => {
     dispatch({ type: DND_TASK_BEGIN });
     try {
-      const { data } = await axios.patch(
-        `api/v1/boards/board/${state.activeBoardId}/tasks/dnd`,
-        values,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
+      const { data } = await axiosInstance.patch(
+        `/board/${activeBoardId}/tasks/dnd`,
+        values
       );
       dispatch({ type: DND_TASK_SUCCESS, payload: data });
     } catch (error) {
       dispatch({ type: DND_TASK_ERROR });
-      if (error.request.status === 401) {
-        logoutUser();
-      }
-    }
-  };
-
-  const clearDemoData = async () => {
-    try {
-      await axios.get('api/v1/boards/clearDemo', {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-    } catch (error) {
-      if (error.request.status === 401) {
-        logoutUser();
-      }
     }
   };
 
@@ -369,28 +247,17 @@ export const BoardProvider = ({ children }) => {
       value={{
         ...state,
         createBoard,
-        handleDeleteTaskModal,
-        handleCreateBoardModal,
-        handleSideBoardModal,
-        closeModal,
         getUserBoards,
-        getActiveBoardId,
-        handleDeleteBoardModal,
+        setActiveBoardId,
         deleteBoard,
         editBoard,
-        handleEditBoardModal,
-        handleAddColumnModal,
-        handleAddTaskModal,
         addNewTask,
-        handleViewTaskModal,
         getCurrentTask,
         editSubTaskCheckBox,
         moveTasks,
         deleteTask,
-        handleEditTaskModal,
         editTask,
         dndTask,
-        clearDemoData,
       }}
     >
       {children}
---
+++ client/src/context/modalsContext.jsx
@@ -0,0 +1,115 @@
+import React, { useContext, useState } from 'react';
+
+const initialState = {
+  asideIsVisible: true,
+  createBoardVisible: false,
+  sideBoardModalVisible: false,
+  editBoardVisible: false,
+  deleteModalVisible: false,
+  addColumnModalVisible: false,
+  addTaskModalVisible: false,
+  viewTaskModalVisible: false,
+  deleteTaskModalVisible: false,
+  updateTaskModalVisible: false,
+};
+
+const ModalsContext = React.createContext();
+
+export const ModalProvider = ({ children }) => {
+  const [state, setState] = useState(initialState);
+
+  const asideVisibilityToggle = () => {
+    setState({ ...state, asideIsVisible: !state.asideIsVisible });
+  };
+
+  const createBoardsModalToggle = () => {
+    setState({ ...state, createBoardVisible: !state.createBoardVisible });
+  };
+
+  const sideBoardsModalToggle = () => {
+    setState({ ...state, sideBoardModalVisible: !state.sideBoardModalVisible });
+  };
+
+  const editBoardsModalToggle = () => {
+    setState({
+      ...state,
+      editBoardVisible: !state.editBoardVisible,
+      sideBoardModalVisible: false,
+    });
+  };
+
+  const deleteBoardsModalToggle = () => {
+    setState({
+      ...state,
+      deleteModalVisible: !state.deleteModalVisible,
+      sideBoardModalVisible: false,
+    });
+  };
+
+  const addColumnModalToggle = () => {
+    setState({ ...state, addColumnModalVisible: !state.addColumnModalVisible });
+  };
+
+  const addTaskModalToggle = () => {
+    setState({ ...state, addTaskModalVisible: !state.addTaskModalVisible });
+  };
+
+  const viewTaskModalToggle = () => {
+    setState({ ...state, viewTaskModalVisible: !state.viewTaskModalVisible });
+  };
+
+  const deleteTaskModalToggle = () => {
+    setState({
+      ...state,
+      deleteTaskModalVisible: !state.deleteTaskModalVisible,
+      viewTaskModalVisible: false,
+    });
+  };
+
+  const editTaskModalToggle = () => {
+    setState({
+      ...state,
+      updateTaskModalVisible: !state.updateTaskModalVisible,
+      viewTaskModalVisible: false,
+    });
+  };
+
+  const closeModals = () => {
+    setState({
+      ...state,
+      createBoardVisible: false,
+      sideBoardModalVisible: false,
+      editBoardVisible: false,
+      deleteModalVisible: false,
+      addColumnModalVisible: false,
+      addTaskModalVisible: false,
+      viewTaskModalVisible: false,
+      deleteTaskModalVisible: false,
+      updateTaskModalVisible: false,
+    });
+  };
+
+  return (
+    <ModalsContext.Provider
+      value={{
+        ...state,
+        closeModals,
+        asideVisibilityToggle,
+        createBoardsModalToggle,
+        sideBoardsModalToggle,
+        editBoardsModalToggle,
+        deleteBoardsModalToggle,
+        addColumnModalToggle,
+        addTaskModalToggle,
+        viewTaskModalToggle,
+        deleteTaskModalToggle,
+        editTaskModalToggle,
+      }}
+    >
+      {children}
+    </ModalsContext.Provider>
+  );
+};
+export const useModalContext = () => {
+  return useContext(ModalsContext);
+};
