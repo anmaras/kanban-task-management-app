ce2e9d908b30099a1de630a0cce3bfb3bb34265d
Anthony Maras <marasantonis@gmail.com>
2023-02-26 19:28:25 +0200

    file extention updated

---
+++ client/src/components/index.jsx
@@ -0,0 +1,35 @@
+import Form from './form/Form';
+import AuthLayout from './layouts/authLayout/AuthLayout';
+import HomeLayout from './layouts/homeLayout/HomeLayout';
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout';
+import EditButton from './editButton/EditButton';
+import Modal from './modal/Modal';
+import Spinner from './spinner/Spinner';
+import Loader from './loader/Loader';
+import PortalComponents from './portal/PortalComponents';
+import Header from '../components/dashboard/header/Header';
+import Aside from './dashboard/aside/Aside';
+import AsideBoardsCard from './dashboard/aside/asideBoardsCard/AsideBoardsCard';
+import Options from './dashboard/options/Options';
+import List from '../components/dashboard/columns/list/List';
+import Tasks from './dashboard/tasks/Tasks';
+import Table from './dashboard/columns/table/Table';
+
+export {
+  Table,
+  Tasks,
+  List,
+  Options,
+  Aside,
+  AsideBoardsCard,
+  DashboardLayout,
+  Form,
+  EditButton,
+  AuthLayout,
+  HomeLayout,
+  Modal,
+  Spinner,
+  Loader,
+  PortalComponents,
+  Header,
+};
