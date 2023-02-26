050d798a840474a294dcd9ef55082e39c744f7a2
Anthony Maras <marasantonis@gmail.com>
2023-02-26 19:16:22 +0200

    refactor routes

--- server/routes/boardRoutes.js
+++ server/routes/boardRoutes.js
@@ -14,28 +14,22 @@ import {
   dndTask,
 } from '../controllers/boardController.js';
 
+router.route('/user-boards').get(getAllBoards);
 router.route('/create-board').post(createBoard);
-router.route('/get-user-boards').get(getAllBoards);
-router
-  .route('/get-user-boards/board/:id')
-  .delete(deleteBoard)
-  .patch(updateBoard);
-router
-  .route('/get-user-boards/board/:id/column/:columnId')
-  .post(createBoardTask);
+router.route('/active/board/:id').patch(selectActive);
+router.route('/board/:id').delete(deleteBoard).patch(updateBoard);
+router.route('/board/:id/column/:columnId').post(createBoardTask);
 
-router.route('/get-user-boards/active/board/:id').patch(selectActive);
 router
   .route('/board/:boardId/column/:columnId/task/:taskId/subtask/:subId')
   .patch(editSubtask);
 router
   .route('/board/:boardId/column/:fromId/task/:taskId/move')
   .patch(moveTask);
+
 router
-  .route('/board/:boardId/column/:columnId/task/:taskId/delete')
-  .delete(deleteTask);
-router
-  .route('/board/:boardId/column/:columnId/task/:taskId/edit')
+  .route('/board/:boardId/column/:columnId/task/:taskId')
+  .delete(deleteTask)
   .patch(editTask);
 
 router.route('/board/:boardId/tasks/dnd').patch(dndTask);
