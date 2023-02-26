b66666aa88f36397234564e1d3d8383484addc80
Anthony Maras <marasantonis@gmail.com>
2023-02-26 19:15:26 +0200

    add modal provider

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import './styles/index.scss';
 import App from './App';
 import { UserProvider } from './context/userContext';
 import { BoardProvider } from './context/boardsContext';
+import { ModalProvider } from './context/modalsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <BoardProvider>
-        <App />
-      </BoardProvider>
+      <ModalProvider>
+        <BoardProvider>
+          <App />
+        </BoardProvider>
+      </ModalProvider>
     </UserProvider>
   </React.StrictMode>
 );
