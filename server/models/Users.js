3355a1bd599da8f3dd645098c09d9dced5027a6f
Anthony Maras <marasantonis@gmail.com>
2023-03-01 14:05:38 +0200

    remove validation lib

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import validator from 'validator';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
@@ -14,10 +13,6 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please provide email'],
-    validate: {
-      validator: validator.isEmail,
-      message: 'Please provide a valid email',
-    },
     unique: true,
   },
 
