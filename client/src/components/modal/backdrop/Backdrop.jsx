cc65bdf3ce3b5f3d0a09faf00a846aaff9bb9098
Anthony Maras <marasantonis@gmail.com>
2023-03-01 20:12:54 +0200

    remove animation

--- client/src/components/modal/backdrop/Backdrop.jsx
+++ client/src/components/modal/backdrop/Backdrop.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { useModalContext } from '../../../context/modalsContext';
 import style from './Backdrop.module.scss';
-import { motion } from 'framer-motion';
 
 const Backdrop = ({ children }) => {
   const { closeModals } = useModalContext();
   return (
-    <motion.div
+    <div
       className={style.backdrop}
-      key="modal"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.2 }}
       onDoubleClick={(e) => {
         if (e.target === e.currentTarget) {
           //need to stop further propagation so modals to work
@@ -22,7 +16,7 @@ const Backdrop = ({ children }) => {
       }}
     >
       {children}
-    </motion.div>
+    </div>
   );
 };
 
