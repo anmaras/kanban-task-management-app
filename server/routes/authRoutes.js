e2536090a0a17658a6a8bb95a6a7dcb8b2f0368b
Anthony Maras <marasantonis@gmail.com>
2023-03-01 20:20:02 +0200

    delete user route added

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 const router = express.Router();
-import { register, login, updateUser } from '../controllers/authController.js';
+import {
+  register,
+  login,
+  updateUser,
+  deleteAccount,
+} from '../controllers/authController.js';
 import authenticatedUser from '../middleware/auth.js';
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/updateUser').patch(authenticatedUser, updateUser);
+router.route('/user/:userId').delete(authenticatedUser, deleteAccount);
 
 export default router;
