0eb136cf5bc0ba35aed216061b56d883a207cd00
Anthony Maras <marasantonis@gmail.com>
2023-03-06 18:37:19 +0200

    add features constants

--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -18,3 +18,22 @@ export const formInputs = [
     placeholder: 'Your password here',
   },
 ];
+
+export const features = [
+  {
+    id: 0,
+    title: 'Drag and Drop Interface',
+    content: 'Easily move tasks between columns with drag and drop interface.',
+  },
+  {
+    id: 1,
+    title: 'Customizable Boards',
+    content:
+      'Organize your tasks on personalized boards that reflect your workflow. Add, edit or remove boards columns and tasks to fit your needs.',
+  },
+  {
+    id: 2,
+    title: 'Save on Database',
+    content: 'Save your boards and tasks on a secure database',
+  },
+];
