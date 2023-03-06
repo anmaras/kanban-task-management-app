dcd9bd688fb40fee24dc2823f136b54cc04ae433
Anthony Maras <marasantonis@gmail.com>
2023-03-06 18:36:51 +0200

    add features

--- client/src/pages/landingPage/LandingPage.jsx
+++ client/src/pages/landingPage/LandingPage.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { HomeLayout } from '../../components/index';
 import Typewriter from 'typewriter-effect';
 import style from './LandingPage.module.scss';
+import { features } from '../../utils/constants';
+import { animate, motion } from 'framer-motion';
 
 const LandingPage = () => {
   return (
@@ -23,7 +25,6 @@ const LandingPage = () => {
                 .pauseFor(2500)
                 .deleteAll()
                 .typeString('boards.')
-                .pauseFor(2500)
                 .deleteAll()
                 .typeString('columns.')
                 .pauseFor(2500)
@@ -32,13 +33,47 @@ const LandingPage = () => {
             }}
           />
         </h1>
-        <p className={style['landingPage__subtitle']}>
-          Kanban will provide a simple yet effective solution for managing
+        <ul className={style['landingPage__features']}>
+          {features.map((item, index) => {
+            const { id, title, content } = item;
+            return (
+              <motion.li
+                key={id}
+                className={style['landingPage__feature']}
+                initial={{ opacity: 0, x: '-100%' }}
+                animate={{
+                  opacity: 1,
+                  x: 0,
+                  transition: {
+                    type: 'spring',
+                    delay: 0.2 * index,
+                    duration: 1,
+                  },
+                }}
+              >
+                <h3 className="heading-M">{title}</h3>
+                <p>{content}</p>
+              </motion.li>
+            );
+          })}
+        </ul>
+        <motion.p
+          className={style['landingPage__subtitle']}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 2, delay: 0.75 } }}
+        >
+          Kanban will provide a simple yet effective solution for managing your
           projects and tasks.
-        </p>
-        <Link to="/login" className="button button--auth">
-          Get Started
-        </Link>
+        </motion.p>
+        <motion.div
+          className={style['landingPage__btn-container']}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 2, delay: 1 } }}
+        >
+          <Link to="/login" className="button button--auth">
+            Get Started
+          </Link>
+        </motion.div>
       </div>
     </HomeLayout>
   );
